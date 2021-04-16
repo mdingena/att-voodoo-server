943c6ee3ce167e9c92587d7efb134f5682d1afc8
Marc Dingena <marc.dingena@gmail.com>
2021-04-16 08:28:10 +0800

    Rename variable

--- src/api/createApi.ts
+++ src/api/createApi.ts
@@ -6,16 +6,16 @@ import Logger from 'js-tale/dist/logger';
 const port = process.env.PORT || 3000;
 const logger = new Logger('Express');
 
-const webServer = express();
-webServer.use(auth);
-webServer.use(express.json());
+const api = express();
+api.use(auth);
+api.use(express.json());
 
 export const createApi = () => {
-  webServer.post('/token', postAccessToken);
+  api.post('/token', postAccessToken);
 
-  webServer.listen(port, () => {
-    logger.log(`Webserver listening on port ${port}`);
+  api.listen(port, () => {
+    logger.success(`API listening on port ${port}`);
   });
 
-  return webServer;
+  return api;
 };
