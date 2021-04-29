718a965ee561e11a20f8bbb8a56f865b72e0d577
Marc Dingena <marc.dingena@gmail.com>
2021-04-29 08:35:49 +0800

    Move strings directory

--- src/voodoo/spellbook/strings/index.ts
+++ src/voodoo/spellbook/strings/index.ts
@@ -1,4 +1,5 @@
 export { packFloat } from './packFloat';
 export { unpackFloat } from './unpackFloat';
+export { spawnFrom } from './spawnFrom';
 export { tag } from './tag';
 export * from './raw';
