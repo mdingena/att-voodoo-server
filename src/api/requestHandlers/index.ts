ce6cb44f884a5ea577a845a1f4bf9b6606e99192
Marc Dingena <marc.dingena@gmail.com>
2021-05-06 22:29:54 +0800

    Add GET /seal request handler

--- src/api/requestHandlers/index.ts
+++ src/api/requestHandlers/index.ts
@@ -1,5 +1,6 @@
 export { deleteIncantations } from './deleteIncantations';
 export { getHeartbeat } from './getHeartbeat';
+export { getSeal } from './getSeal';
 export { getSession } from './getSession';
 export { postIncantation } from './postIncantation';
 export { postTrigger } from './postTrigger';
