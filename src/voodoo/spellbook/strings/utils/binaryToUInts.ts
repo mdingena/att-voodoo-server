27fb89358b54648b35f0030e5890aef1b0a69776
Marc Dingena <marc.dingena@gmail.com>
2021-05-13 10:04:01 +0800

    Rename binaryToUInts

---
+++ src/voodoo/spellbook/strings/utils/binaryToUInts.ts
@@ -0,0 +1,5 @@
+export const binaryToUInts = (binary: string): string[] => {
+  if (binary.length % 32 !== 0) throw Error('Cannot convert binary to UInts. Bit count is not divisible by 32.');
+
+  return binary.match(/.{32}/g)?.map(words => Number(`0b${words}`).toString()) ?? [];
+};
--- src/voodoo/spellbook/strings/utils/bitsToUInts.ts
+++
@@ -1,2 +0,0 @@
-export const bitsToUInts = (bits: string): string[] =>
-  bits.match(/.{32}/g)?.map(uint => Number(`0b${uint}`).toString()) ?? [];
