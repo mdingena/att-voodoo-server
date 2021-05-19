0da42f4c55eb654e276095548f049f415755aa64
Marc Dingena <marc.dingena@gmail.com>
2021-05-19 11:27:00 +0800

    Change Heroku environment variables

--- src/api/keepAwake.ts
+++ src/api/keepAwake.ts
@@ -5,6 +5,6 @@ const keepAwakeUrl = 'https://att-voodoo-server.herokuapp.com/';
 const keepAwakeInterval = 180000;
 
 export const keepAwake = (voodoo: VoodooServer): NodeJS.Timer | undefined =>
-  !!process.env.ON_HEROKU
+  !!process.env.KEEP_AWAKE
     ? setInterval(() => fetch(keepAwakeUrl).catch(error => voodoo.logger.error(error.message)), keepAwakeInterval)
     : undefined;
