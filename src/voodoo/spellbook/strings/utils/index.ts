00b2b1e74f864bbc305d26554f0b8f3678d3a118
Marc Dingena <marc.dingena@gmail.com>
2021-05-24 09:38:53 +0800

    Fix UInt to binary conversion

--- src/voodoo/spellbook/strings/utils/index.ts
+++ src/voodoo/spellbook/strings/utils/index.ts
@@ -3,4 +3,5 @@ export { binaryToUInts } from './binaryToUInts';
 export { createBinaryReader, BinaryReader } from './createBinaryReader';
 export { numberToBinaryUInt } from './numberToBinaryUInt';
 export { numberToBinary } from './numberToBinary';
+export { uIntToBinary } from './uIntToBinary';
 export { uIntToNumber } from './uIntToNumber';
