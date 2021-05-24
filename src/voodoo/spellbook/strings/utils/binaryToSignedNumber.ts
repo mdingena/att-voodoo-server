59b713ab52e1bda3314eb25aaa13ad4309e0fca0
Marc Dingena <marc.dingena@gmail.com>
2021-05-24 16:37:22 +0800

    Update binaryToSignedNumber.ts

--- src/voodoo/spellbook/strings/utils/binaryToSignedNumber.ts
+++ src/voodoo/spellbook/strings/utils/binaryToSignedNumber.ts
@@ -2,5 +2,5 @@ export const binaryToSignedNumber = (binary: string): number => {
   const isPositive = Boolean(Number(binary.slice(0, 1)));
   const integer = Number(`0b0${binary.slice(1)}`);
 
-  return isPositive ? integer : integer * -1;
+  return isPositive ? integer : integer - 2147483648;
 };
