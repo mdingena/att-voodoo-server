16eddb6cfef28f7f7f3e50e900989ac48c8f0f5d
Marc Dingena <marc.dingena@gmail.com>
2021-05-24 12:14:16 +0800

    Add binary to signed number conversion function

---
+++ src/voodoo/spellbook/strings/utils/binaryToSignedNumber.ts
@@ -0,0 +1,6 @@
+export const binaryToSignedNumber = (binary: string): number => {
+  const isPositive = Boolean(Number(binary.slice(0, 1)));
+  const integer = Number(`0b0${binary.slice(1)}`);
+
+  return isPositive ? integer : integer * -1;
+};
--- src/voodoo/spellbook/strings/utils/index.ts
+++ src/voodoo/spellbook/strings/utils/index.ts
@@ -1,4 +1,5 @@
 export { binaryToNumber } from './binaryToNumber';
+export { binaryToSignedNumber } from './binaryToSignedNumber';
 export { binaryToUIntArray } from './binaryToUIntArray';
 export { createBinaryReader, BinaryReader } from './createBinaryReader';
 export { numberToBinaryUInt } from './numberToBinaryUInt';
