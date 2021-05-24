8e3a14682e95f5589d54bb4a69e999253677daa0
Marc Dingena <marc.dingena@gmail.com>
2021-05-24 16:47:45 +0800

    Add Sentry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { createVoodooServer } from './voodoo';
 import { createBot } from './bot';
 import { createApi, keepAwake } from './api';
+
+if (!!process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0
+  });
+}
 
 (async () => {
   const voodoo = createVoodooServer();
