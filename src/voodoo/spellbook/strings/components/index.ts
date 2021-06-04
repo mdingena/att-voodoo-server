b1707940b0b6907505913f726b310a764f781b02
Marc Dingena <marc.dingena@gmail.com>
2021-06-04 21:40:18 +0800

    Refactor exported component types

--- src/voodoo/spellbook/strings/components/index.ts
+++ src/voodoo/spellbook/strings/components/index.ts
@@ -1,13 +1,19 @@
 import * as transcoders from './transcoders';
 
-export type TranscoderName = keyof typeof transcoders;
-export type TranscoderProperties =
-  //  | transcoders.LiquidContainer.Properties
-  transcoders.NetworkRigidbody.Properties | transcoders.PickupDock.Properties | transcoders.StatManager.Properties;
+export type ComponentName = keyof typeof transcoders;
 
-export type Component = {
-  name: TranscoderName;
-  properties: TranscoderProperties;
+export type KnownComponent =
+  | transcoders.LiquidContainer.Component
+  | transcoders.NetworkRigidbody.Component
+  | transcoders.PickupDock.Component
+  | transcoders.StatManager.Component;
+
+export type UnknownComponent = { hash: number; data: string };
+
+export type Component = KnownComponent | UnknownComponent[];
+
+export type Components = {
+  [key in ComponentName | 'Unknown']?: Component;
 };
 
 export { transcoders };
