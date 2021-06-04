b399ee4e52cf9250f7a39c0d5aa49d920a83fe48
Marc Dingena <marc.dingena@gmail.com>
2021-06-04 12:07:02 +0800

    Add signed integer to binary conversion function

--- src/voodoo/spellbook/strings/utils/index.ts
+++ src/voodoo/spellbook/strings/utils/index.ts
@@ -4,5 +4,6 @@ export { binaryToUIntArray } from './binaryToUIntArray';
 export { createBinaryReader, BinaryReader } from './createBinaryReader';
 export { numberToBinaryUInt } from './numberToBinaryUInt';
 export { numberToBinary } from './numberToBinary';
+export { signedIntegerToBinary } from './signedIntegerToBinary';
 export { uIntToBinary } from './uIntToBinary';
 export { uIntToNumber } from './uIntToNumber';
