5271ff856afacb8e8c184119498c3d465ef1e55e
Marc Dingena <marc.dingena@gmail.com>
2021-06-04 22:23:39 +0800

    Create material spell component parser function

---
+++ src/voodoo/spellbook/strings/parsePrefab.ts
@@ -0,0 +1,46 @@
+import { DecodedString } from './decodeString';
+import { PrefabHash } from './PrefabHash';
+import { parseFlask } from './parsers';
+
+export const parsePrefab = (decoded: DecodedString): string | undefined => {
+  switch (decoded.hash) {
+    /**
+     * FLASK
+     */
+    case PrefabHash.Potion_Medium:
+      return parseFlask(decoded.prefab);
+
+    /**
+     * HILTED APPARATUS
+     */
+    case PrefabHash.Curled_Wooden_Handle:
+    case PrefabHash.Handle_Bow:
+    case PrefabHash.Handle_Fist:
+    case PrefabHash.Handle_Large_Branch:
+    case PrefabHash.Handle_Large_Cool:
+    case PrefabHash.Handle_Large_Standard:
+    case PrefabHash.Handle_Long_Straight:
+    case PrefabHash.Handle_Medium_Branch:
+    case PrefabHash.Handle_Medium_Cool:
+    case PrefabHash.Handle_Medium_Curved:
+    case PrefabHash.Handle_Medium_Ridged:
+    case PrefabHash.Handle_Medium_Standard:
+    case PrefabHash.Handle_Medium_Straight:
+    case PrefabHash.Handle_Round_Fist:
+    case PrefabHash.Handle_Short:
+    case PrefabHash.Handle_Short_C_Curve:
+    case PrefabHash.Handle_Short_Cool:
+    case PrefabHash.Handle_Short_Pointy_End:
+    case PrefabHash.Handle_Short_S_Curve:
+    case PrefabHash.Handle_Short_Taper:
+    case PrefabHash.Handle_Spear:
+    case PrefabHash.Handle_Tonfa:
+      return 'hilted apparatus';
+
+    /**
+     * UNRECOGNISED
+     */
+    default:
+      return undefined;
+  }
+};
