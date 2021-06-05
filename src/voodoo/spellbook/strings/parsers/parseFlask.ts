435b1bc70d7592c4cdbf95f87fe79a22812a86e2
Marc Dingena <marc.dingena@gmail.com>
2021-06-05 14:35:49 +0800

    Add water to flask parser

--- src/voodoo/spellbook/strings/parsers/parseFlask.ts
+++ src/voodoo/spellbook/strings/parsers/parseFlask.ts
@@ -3,7 +3,8 @@ import { MaterialSpellComponent } from '../MaterialSpellComponent';
 import { Component } from '../components/transcoders/liquidContainer';
 
 enum PresetHash {
-  TeleportPotion = 27100
+  TeleportPotion = 27100,
+  Water = 44872
 }
 
 export const parseFlask = (prefab: Prefab): string | undefined => {
@@ -12,6 +13,8 @@ export const parseFlask = (prefab: Prefab): string | undefined => {
   if (!component) return undefined;
 
   if (!component.hasContent) return MaterialSpellComponent.FlaskEmpty;
+
+  if (component.presetHash === PresetHash.Water) return MaterialSpellComponent.FlaskWater;
 
   if (component.presetHash === PresetHash.TeleportPotion) return MaterialSpellComponent.FlaskTeleportPotion;
 
