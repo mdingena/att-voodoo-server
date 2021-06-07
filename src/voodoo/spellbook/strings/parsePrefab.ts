511279f2f44fefe6d97895bced53ac1227e58d02
Marc Dingena <marc.dingena@gmail.com>
2021-06-07 16:04:37 +0800

    Add material spell component parsers

--- src/voodoo/spellbook/strings/parsePrefab.ts
+++ src/voodoo/spellbook/strings/parsePrefab.ts
@@ -5,18 +5,124 @@ import { parseFlask, parseLeather } from './parsers';
 export const parsePrefab = (decoded: DecodedString): string | undefined => {
   switch (decoded.hash) {
     /**
-     * FLASK
+     * COAL
      */
-    case PrefabHash.Potion_Medium:
-      return parseFlask(decoded.prefab);
+    case PrefabHash.Coal:
+      return 'coal';
 
     /**
-     * LEATHER
+     * SANDSTONE
      */
-    case PrefabHash.Soft_Fabric_Medium_Strips:
-    case PrefabHash.Soft_Fabric_Medium_Roll:
-    case PrefabHash.Soft_Fabric_Large_Roll:
-      return parseLeather(decoded.prefab);
+    case PrefabHash.Sandstone_Stone:
+      return 'sandstone';
+
+    /**
+     * SALT
+     */
+    case PrefabHash.Salt:
+      return 'salt';
+
+    /**
+     * DYNAMITE
+     */
+    case PrefabHash.Dynamite:
+      return 'dynamite';
+
+    /**
+     * FIREWORK
+     */
+    case PrefabHash.Firework:
+      return 'firework';
+
+    /**
+     * CRYSTAL
+     */
+    case PrefabHash.Crystal_Gem_Blue:
+      return 'blue crystal gem';
+
+    case PrefabHash.Crystal_Shard_Blue:
+      return 'blue crystal shard';
+
+    /**
+     * FEATHER
+     */
+    case PrefabHash.Spriggull_Feather_Blue:
+      return 'blue spriggull feather';
+
+    case PrefabHash.Spriggull_Feather_Red:
+      return 'red spriggull feather';
+
+    /**
+     * MUSHROOM
+     */
+    case PrefabHash.MushroomBrown_Full_Burnt:
+      return 'burnt brown mushroom';
+
+    case PrefabHash.MushroomBrown_Full_Cooked:
+      return 'cooked brown mushroom';
+
+    case PrefabHash.MushroomBrown_Full_Ripe:
+      return 'ripe brown mushroom';
+
+    case PrefabHash.MushroomCaveSmall_Full_Burnt:
+      return 'burnt cave mushroom';
+
+    case PrefabHash.MushroomCaveSmall_Full_Cooked:
+      return 'cooked cave mushroom';
+
+    case PrefabHash.MushroomCaveSmall_Full_Ripe:
+      return 'ripe cave mushroom';
+
+    case PrefabHash.MushroomGlowing_Full_Burnt:
+      return 'burnt glowing mushroom';
+
+    case PrefabHash.MushroomGlowing_Full_Cooked:
+      return 'cooked glowing mushroom';
+
+    case PrefabHash.MushroomGlowing_Full_Ripe:
+      return 'ripe glowing mushroom';
+
+    case PrefabHash.MushroomRed_Full_Burnt:
+      return 'burnt red mushroom';
+
+    case PrefabHash.MushroomRed_Full_Cooked:
+      return 'cooked red mushroom';
+
+    case PrefabHash.MushroomRed_Full_Ripe:
+      return 'ripe red mushroom';
+
+    /**
+     * INGOT
+     */
+    case PrefabHash.Carsi_Ingot:
+      return 'palladium ingot';
+
+    case PrefabHash.Copper_Ingot:
+      return 'copper ingot';
+
+    case PrefabHash.Evinon_Steel_Ingot:
+      return 'valyan ingot';
+
+    case PrefabHash.Gold_Ingot:
+      return 'gold ingot';
+
+    case PrefabHash.Iron_Ingot:
+      return 'iron ingot';
+
+    case PrefabHash.Mythril_Ingot:
+      return 'mythril ingot';
+
+    case PrefabHash.Orchi_Ingot:
+      return 'virdium ingot';
+
+    case PrefabHash.Red_Iron_Ingot:
+      return 'red iron ingot';
+
+    case PrefabHash.Silver_Ingot:
+      return 'silver ingot';
+
+    case PrefabHash.White_Gold_Ingot:
+      return 'electrum ingot';
 
     /**
      * HILTED APPARATUS
@@ -44,6 +150,20 @@ export const parsePrefab = (decoded: DecodedString): string | undefined => {
     case PrefabHash.Handle_Spear:
     case PrefabHash.Handle_Tonfa:
       return 'hilted apparatus';
+
+    /**
+     * FLASK
+     */
+    case PrefabHash.Potion_Medium:
+      return parseFlask(decoded.prefab);
+
+    /**
+     * LEATHER
+     */
+    case PrefabHash.Soft_Fabric_Medium_Strips:
+    case PrefabHash.Soft_Fabric_Medium_Roll:
+    case PrefabHash.Soft_Fabric_Large_Roll:
+      return parseLeather(decoded.prefab);
 
     /**
      * UNRECOGNISED
