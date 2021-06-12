25a9bc4fb6af6f8a54ec9396b3019bc19b4cd63e
Marc Dingena <marc.dingena@gmail.com>
2021-06-12 14:47:42 +0800

    Refactor connection event handling

--- src/bot/serverConnectionHandlers.ts
+++ src/bot/serverConnectionHandlers.ts
@@ -1,41 +1,73 @@
-import ServerConnection from 'js-tale/dist/Groups/ServerConnection';
+import { Server, ServerConnection } from 'js-tale';
 import Logger from 'js-tale/dist/logger';
 import { VoodooServer } from '../voodoo';
 
 const logger = new Logger('Bot');
 
 export const handleServerConnectionOpened = (voodoo: VoodooServer) => async (connection: ServerConnection) => {
-  logger.success(`Connected to ${connection.server.info.name}`);
+  /* Connection closed event handler. */
+  const handleClosed = ({
+    server: {
+      isOnline,
+      info: { id, name, online_players }
+    }
+  }: ServerConnection) => {
+    voodoo.removePlayers({ serverId: id });
+    voodoo.updateServer({
+      id,
+      name,
+      online: isOnline,
+      players: online_players.length
+    });
 
-  connection.server.on('update', server => {
+    logger.warn(`Disconnected from ${name}`);
+  };
+
+  /* Server info updated event handler. */
+  const handleUpdate = (server: Server) => {
     voodoo.updateServer({
       id: server.info.id,
       name: server.info.name,
       online: server.isOnline,
       players: server.info.online_players.length
     });
-  });
+  };
 
-  connection.subscribe('PlayerJoined', event => {
+  /* Player joined the server event handler. */
+  const handlePlayerJoined = (event: any) => {
     voodoo.addPlayer({
       accountId: event.user.id,
       serverId: connection.server.info.id,
       serverConnection: connection
     });
-  });
+  };
 
-  connection.subscribe('PlayerLeft', event => {
+  /* Player left the server event handler. */
+  const handlePlayerLeft = (event: any) => {
     voodoo.removePlayer({ accountId: event.user.id });
+  };
+
+  /* Register event handlers. */
+  connection.on('closed', handleClosed);
+  connection.subscribe('PlayerJoined', handlePlayerJoined);
+  connection.subscribe('PlayerLeft', handlePlayerLeft);
+  connection.server.on('update', handleUpdate);
+
+  /* Update server info immediately after connecting. */
+  voodoo.updateServer({
+    id: connection.server.info.id,
+    name: connection.server.info.name,
+    online: connection.server.isOnline,
+    players: connection.server.info.online_players.length
   });
 
-  connection.on('closed', handleServerConnectionClosed(voodoo));
-
+  /* Add all players on the server immediately after connecting. */
   try {
     const { Result: players } = await connection.send('player list');
 
-    players.map((item: any) =>
+    players.map((player: any) =>
       voodoo.addPlayer({
-        accountId: item.id,
+        accountId: player.id,
         serverId: connection.server.info.id,
         serverConnection: connection
       })
@@ -43,18 +75,6 @@ export const handleServerConnectionOpened = (voodoo: VoodooServer) => async (con
   } catch (error) {
     logger.error(error);
   }
-};
 
-export const handleServerConnectionClosed = (voodoo: VoodooServer) => (connection: ServerConnection) => {
-  const { server } = connection;
-
-  logger.warn(`Disconnected from ${server.info.name}`);
-
-  voodoo.removePlayers({ serverId: server.info.id });
-  voodoo.updateServer({
-    id: server.info.id,
-    name: server.info.name,
-    online: server.isOnline,
-    players: server.info.online_players.length
-  });
+  logger.success(`Connected to ${connection.server.info.name}`);
 };
