4404844fa3c2f4f9fb5ed76aa61bc1042329ac82
Marc Dingena <marc.dingena@gmail.com>
2021-06-14 15:50:59 +0800

    Export new spells

--- src/voodoo/spellbook/spells/index.ts
+++ src/voodoo/spellbook/spells/index.ts
@@ -1,4 +1,7 @@
 import { acidBolt } from './acidBolt';
+import { conjureCrystalLance } from './conjureCrystalLance';
+import { conjureCrystalPick } from './conjureCrystalPick';
+import { conjureCrystalSword } from './conjureCrystalSword';
 import { conjureTeleportationPotion } from './conjureTeleportationPotion';
 import { conjureWater } from './conjureWater';
 import { craftFlask } from './craftFlask';
@@ -6,14 +9,35 @@ import { flaskOfEndlessTeleportation } from './flaskOfEndlessTeleportation';
 import { flaskOfEndlessWater } from './flaskOfEndlessWater';
 import { frostBolt } from './frostBolt';
 import { repairCopperWeapon } from './repairCopperWeapon';
+import { repairElectrumWeapon } from './repairElectrumWeapon';
+import { repairGoldWeapon } from './repairGoldWeapon';
+import { repairIronWeapon } from './repairIronWeapon';
+import { repairMythrilWeapon } from './repairMythrilWeapon';
+import { repairPalladiumWeapon } from './repairPalladiumWeapon';
+import { repairRedIronWeapon } from './repairRedIronWeapon';
+import { repairSilverWeapon } from './repairSilverWeapon';
+import { repairValyanWeapon } from './repairValyanWeapon';
+import { repairViridiumWeapon } from './repairViridiumWeapon';
 
 export const spells = {
   acidBolt,
+  conjureCrystalLance,
+  conjureCrystalPick,
+  conjureCrystalSword,
   conjureTeleportationPotion,
   conjureWater,
   craftFlask,
   flaskOfEndlessTeleportation,
   flaskOfEndlessWater,
   frostBolt,
-  repairCopperWeapon
+  repairCopperWeapon,
+  repairElectrumWeapon,
+  repairGoldWeapon,
+  repairIronWeapon,
+  repairMythrilWeapon,
+  repairPalladiumWeapon,
+  repairRedIronWeapon,
+  repairSilverWeapon,
+  repairValyanWeapon,
+  repairViridiumWeapon
 };
