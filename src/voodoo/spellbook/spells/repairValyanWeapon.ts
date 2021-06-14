752fbc5223bd30024fe18b741088cf37d1824a56
Marc Dingena <marc.dingena@gmail.com>
2021-06-14 15:46:33 +0800

    Add more weapon repair spells

---
+++ src/voodoo/spellbook/spells/repairValyanWeapon.ts
@@ -0,0 +1,31 @@
+import { VoodooServer } from '../..';
+import { spawn } from '../spawn';
+import { spawnFrom } from '../spawnFrom';
+import { repairMaterial } from '../strings/utils';
+import { MaterialHash } from '../strings/MaterialHash';
+
+export const repairValyanWeapon = async (voodoo: VoodooServer, accountId: number): Promise<void> => {
+  const hiltedApparatus = voodoo.players[accountId].incantations[0].decodedString.prefab;
+
+  const player = await voodoo.getPlayerDetailed({ accountId });
+
+  const { position, rotation } = spawnFrom(player, 'rightPalm', 0.05);
+
+  const repairedApparatus = repairMaterial(hiltedApparatus, MaterialHash.EvinonSteelAlloy, 0.25);
+
+  return spawn(voodoo, accountId, {
+    ...repairedApparatus,
+    prefabObject: {
+      ...repairedApparatus.prefabObject,
+      position,
+      rotation
+    },
+    components: {
+      ...repairedApparatus.components,
+      NetworkRigidbody: {
+        position,
+        rotation
+      }
+    }
+  });
+};
