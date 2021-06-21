10a5e96e23b8cd52886b0f4c8835325ef4b1ffac
Marc Dingena <marc.dingena@gmail.com>
2021-06-21 22:13:36 +0800

    Add player API endpoint

--- src/api/requestHandlers/index.ts
+++ src/api/requestHandlers/index.ts
@@ -1,7 +1,8 @@
 export { deleteIncantations } from './deleteIncantations';
+export { getHeartbeat } from './getHeartbeat';
 export { getInfo } from './getInfo';
+export { getPlayer } from './getPlayer';
 export { getSeal } from './getSeal';
 export { getSession } from './getSession';
-export { getHeartbeat } from './getHeartbeat';
 export { postIncantation } from './postIncantation';
 export { postTrigger } from './postTrigger';
