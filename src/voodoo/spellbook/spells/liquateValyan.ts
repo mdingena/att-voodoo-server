82c8a76177bdec5f06be647e7ee241838c6f6066
Marc Dingena <marc.dingena@gmail.com>
2021-06-22 15:30:29 +0800

    Add transmutation spells

---
+++ src/voodoo/spellbook/spells/liquateValyan.ts
@@ -0,0 +1,41 @@
+import { VoodooServer } from '../..';
+import { PrefabHash } from '../strings';
+import { spawn } from '../spawn';
+import { spawnFrom } from '../spawnFrom';
+
+export const liquateValyan = async (voodoo: VoodooServer, accountId: number): Promise<void> => {
+  const player = await voodoo.getPlayerDetailed({ accountId });
+
+  const { position: positionLeft, rotation: rotationLeft } = spawnFrom(player, 'leftPalm', 0.05);
+  const { position: positionRight, rotation: rotationRight } = spawnFrom(player, 'rightPalm', 0.05);
+
+  spawn(voodoo, accountId, {
+    prefabObject: {
+      hash: PrefabHash.Mythril_Ingot,
+      position: positionLeft,
+      rotation: rotationLeft
+    },
+    components: {
+      NetworkRigidbody: {
+        position: positionLeft,
+        rotation: rotationLeft
+      }
+    }
+  });
+
+  spawn(voodoo, accountId, {
+    prefabObject: {
+      hash: PrefabHash.Silver_Ingot,
+      position: positionRight,
+      rotation: rotationRight
+    },
+    components: {
+      NetworkRigidbody: {
+        position: positionRight,
+        rotation: rotationRight
+      }
+    }
+  });
+
+  return;
+};
---
+++ src/voodoo/spellbook/spells/transmuteGoldToSilver.ts
@@ -0,0 +1,24 @@
+import { VoodooServer } from '../..';
+import { PrefabHash } from '../strings';
+import { spawn } from '../spawn';
+import { spawnFrom } from '../spawnFrom';
+
+export const transmuteGoldToSilver = async (voodoo: VoodooServer, accountId: number): Promise<void> => {
+  const player = await voodoo.getPlayerDetailed({ accountId });
+
+  const { position, rotation } = spawnFrom(player, 'rightPalm', 0.05);
+
+  return spawn(voodoo, accountId, {
+    prefabObject: {
+      hash: PrefabHash.Silver_Ingot,
+      position,
+      rotation
+    },
+    components: {
+      NetworkRigidbody: {
+        position,
+        rotation
+      }
+    }
+  });
+};
