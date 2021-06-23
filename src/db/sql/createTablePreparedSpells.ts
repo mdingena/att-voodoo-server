9625ed95e2c5ff60c1a3894c8938783167c4970c
Marc Dingena <marc.dingena@gmail.com>
2021-06-23 13:27:24 +0800

    Update stored table creation SQL

--- src/db/sql/createTablePreparedSpells.ts
+++ src/db/sql/createTablePreparedSpells.ts
@@ -1,8 +1,8 @@
 const createTablePreparedSpells = `
 CREATE TABLE prepared_spells (
-  account_id integer not null,
-  server_id integer not null,
-  prepared_spells text not null
+  account_id INTEGER NOT NULL,
+  server_id INTEGER NOT NULL,
+  prepared_spells TEXT NOT NULL
 );
 
 CREATE UNIQUE INDEX ON prepared_spells (account_id, server_id);
