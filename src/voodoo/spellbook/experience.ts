6138a641d6ae371e6e0c0f8d8d86dfef316cb408
Marc Dingena <marc.dingena@gmail.com>
2021-06-28 16:37:07 +0800

    Get all the upgraded attributes of a given spell

--- src/voodoo/spellbook/experience.ts
+++ src/voodoo/spellbook/experience.ts
@@ -1,5 +1,30 @@
+type UpgradeConfig = {
+  isStepFunction: boolean;
+  min: number;
+  max: number;
+  constant: number;
+};
+
+type SpellUpgrades = { [key: string]: number };
+
+type UpgradeConfigs = { [key: string]: UpgradeConfig };
+
+const upgradedAttribute = (
+  upgrades: number | undefined,
+  { isStepFunction, min, max, constant }: UpgradeConfig
+): number => {
+  const attribute = max - (max - min) * Math.exp(-constant * (upgrades ?? 0));
+  return isStepFunction ? Math.round(attribute) : attribute;
+};
+
 export const xpGain = (incantations: number): number => {
   const xp = -10 + (incantations - 1) * 20;
 
   return Math.max(0, xp);
 };
+
+export const getSpellAttributes = (spellUpgrades: SpellUpgrades, configs: UpgradeConfigs): SpellUpgrades =>
+  Object.keys(configs).reduce(
+    (sum, key) => ({ ...sum, [key]: upgradedAttribute(spellUpgrades[key], configs[key]) }),
+    {}
+  );
