79d39165baf8fca5fe732445de41ce3614523b9f
Marc Dingena <marc.dingena@gmail.com>
2021-07-04 23:06:33 +0800

    Update conjureCrystalLance.ts

--- src/voodoo/spellbook/spells/conjureCrystalLance.ts
+++ src/voodoo/spellbook/spells/conjureCrystalLance.ts
@@ -1,24 +1,48 @@
-import { VoodooServer } from '../..';
+import { SpellFunction } from '../spellbook';
+import { getSpellAttributes } from '../experience';
+import { spawnFrom } from '../spawnFrom';
 import { PrefabHash } from '../strings';
 import { spawn } from '../spawn';
-import { spawnFrom } from '../spawnFrom';
 
-export const conjureCrystalLance = async (voodoo: VoodooServer, accountId: number): Promise<void> => {
+export const conjureCrystalLance: SpellFunction = async (voodoo, accountId, upgradeConfigs): Promise<void> => {
+  const upgrades = voodoo.getSpellUpgrades({ accountId, spell: 'conjureCrystalLance' });
+  const attributes = getSpellAttributes(upgrades, upgradeConfigs);
+
   const player = await voodoo.getPlayerDetailed({ accountId });
-
-  const { position, rotation } = spawnFrom(player, 'rightPalm', 0.05);
+  const rightHand = spawnFrom(player, 'rightPalm', 0.05);
 
   spawn(voodoo, accountId, {
     prefabObject: {
       hash: PrefabHash.Crystal_Lance_Blue,
-      position,
-      rotation
+      position: rightHand.position,
+      rotation: rightHand.rotation
     },
     components: {
       NetworkRigidbody: {
-        position,
-        rotation
+        position: rightHand.position,
+        rotation: rightHand.rotation
       }
     }
   });
+
+  if (attributes.ambidextrous === 2) {
+    const leftHand = spawnFrom(player, 'leftPalm', 0.05);
+
+    spawn(voodoo, accountId, {
+      prefabObject: {
+        hash: PrefabHash.Crystal_Lance_Blue,
+        position: leftHand.position,
+        rotation: leftHand.rotation
+      },
+      components: {
+        NetworkRigidbody: {
+          position: leftHand.position,
+          rotation: leftHand.rotation
+        }
+      }
+    });
+  }
+
+  const { name, serverId, serverName } = voodoo.players[accountId];
+  voodoo.logger.success(`[${serverName ?? serverId} | ${name}] cast Conjure Crystal Lance`);
 };
