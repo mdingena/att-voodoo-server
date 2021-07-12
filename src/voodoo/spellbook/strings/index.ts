3008f429c0b2e1d7fa690c99e4353d577b8c3453
Marc Dingena <marc.dingena@gmail.com>
2021-07-12 14:51:00 +0800

    Add PrefabEmbeddedEntityHash

--- src/voodoo/spellbook/strings/index.ts
+++ src/voodoo/spellbook/strings/index.ts
@@ -3,4 +3,5 @@ export { createString } from './createString';
 export { decodeString, DecodedString } from './decodeString';
 export { parsePrefab } from './parsePrefab';
 export { PrefabHash } from './PrefabHash';
+export { PrefabEmbeddedEntityHash } from './PrefabEmbeddedEntityHash';
 export { PresetHash } from './PresetHash';
