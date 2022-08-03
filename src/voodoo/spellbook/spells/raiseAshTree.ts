9688cafcc95f49dec4a267b3286281d010044fb8
Marc Dingena <marc.dingena@gmail.com>
2022-08-03 16:23:03 +0800

    Add more raise tree spells

---
+++ src/voodoo/spellbook/spells/raiseAshTree.ts
@@ -0,0 +1,25 @@
+import { SpellFunction } from '../spellbook';
+import { getSpellAttributes } from '../experience';
+import { EvokeAngle, EvokeHandedness, spawnFrom } from '../spawnFrom';
+import { createRandomTree, SpeciesHash } from 'att-string-transcoder';
+import { spawn } from '../spawn';
+
+export const raiseAshTree: SpellFunction = async (voodoo, accountId, upgradeConfigs) => {
+  const upgrades = voodoo.getSpellUpgrades({ accountId, spell: 'raiseAshTree' });
+  const attributes = getSpellAttributes(upgrades, upgradeConfigs);
+
+  const terminationRate = 1 / attributes.arborist;
+
+  const player = await voodoo.getPlayerDetailed({ accountId });
+  const dexterity = voodoo.players[accountId].dexterity.split('/') as [EvokeHandedness, EvokeAngle];
+  const mainHand = spawnFrom(player, 'mainHand', dexterity, 0.5);
+
+  const tree = createRandomTree(SpeciesHash.Ash, terminationRate, mainHand.position);
+
+  spawn(voodoo, accountId, tree);
+
+  voodoo.command({ accountId, command: 'select snap-ground' });
+
+  const { name, serverId, serverName } = voodoo.players[accountId];
+  console.log(`[${serverName ?? serverId} | ${name}] cast Raise Ash Tree`);
+};
---
+++ src/voodoo/spellbook/spells/raiseWalnutTree.ts
@@ -0,0 +1,25 @@
+import { SpellFunction } from '../spellbook';
+import { getSpellAttributes } from '../experience';
+import { EvokeAngle, EvokeHandedness, spawnFrom } from '../spawnFrom';
+import { createRandomTree, SpeciesHash } from 'att-string-transcoder';
+import { spawn } from '../spawn';
+
+export const raiseWalnutTree: SpellFunction = async (voodoo, accountId, upgradeConfigs) => {
+  const upgrades = voodoo.getSpellUpgrades({ accountId, spell: 'raiseWalnutTree' });
+  const attributes = getSpellAttributes(upgrades, upgradeConfigs);
+
+  const terminationRate = 1 / attributes.arborist;
+
+  const player = await voodoo.getPlayerDetailed({ accountId });
+  const dexterity = voodoo.players[accountId].dexterity.split('/') as [EvokeHandedness, EvokeAngle];
+  const mainHand = spawnFrom(player, 'mainHand', dexterity, 0.5);
+
+  const tree = createRandomTree(SpeciesHash.Walnut, terminationRate, mainHand.position);
+
+  spawn(voodoo, accountId, tree);
+
+  voodoo.command({ accountId, command: 'select snap-ground' });
+
+  const { name, serverId, serverName } = voodoo.players[accountId];
+  console.log(`[${serverName ?? serverId} | ${name}] cast Raise Walnut Tree`);
+};
