1e4ec3c6cd602158d01b242a04257005bb8914f6
Marc Dingena <marc.dingena@gmail.com>
2022-11-03 20:29:58 +0800

    Repair devices in main hand

--- src/voodoo/spellbook/spells/repairIronDevice.ts
+++ src/voodoo/spellbook/spells/repairIronDevice.ts
@@ -1,38 +1,52 @@
 import { SpellFunction } from '../spellbook';
 import { getSpellAttributes } from '../experience';
-import { EvokeAngle, EvokeHandedness, spawnFrom } from '../spawnFrom';
-import { repairMaterial } from '../utils';
-import { PhysicalMaterialPartHash } from 'att-string-transcoder';
+import { EvokeAngle, EvokeHandedness } from '../spawnFrom';
+import { parsePrefab, repairMaterial } from '../utils';
+import { decodeString, PhysicalMaterialPartHash } from 'att-string-transcoder';
 import { spawn } from '../spawn';
 
 export const repairIronDevice: SpellFunction = async (voodoo, accountId, upgradeConfigs) => {
-  const hiltedApparatus = voodoo.players[accountId].incantations[0].decodedString.prefab;
-
   const upgrades = voodoo.getSpellUpgrades({ accountId, spell: 'repairIronDevice' });
   const attributes = getSpellAttributes(upgrades, upgradeConfigs);
 
-  const player = await voodoo.getPlayerDetailed({ accountId });
+  const [player, inventory] = await Promise.all([
+    voodoo.getPlayerDetailed({ accountId }),
+    voodoo.getPlayerInventory({ accountId })
+  ]);
 
-  if (typeof player === 'undefined') return;
+  if (typeof player === 'undefined' || typeof inventory === 'undefined') return;
 
-  const dexterity = voodoo.players[accountId].dexterity.split('/') as [EvokeHandedness, EvokeAngle];
-  const { position, rotation } = spawnFrom(player, 'mainHand', [dexterity[0], 'palm'], 0.05);
+  /* Get player main hand content. */
+  const dexterity = voodoo.getDexterity({ accountId }).split('/') as [EvokeHandedness, EvokeAngle];
+  const mainHandKey = dexterity[0] === 'rightHand' ? 'RightHand' : 'LeftHand';
+  const mainHandItemId = inventory[mainHandKey]?.Identifier;
 
+  if (typeof mainHandItemId === 'undefined') return;
+
+  voodoo.command({ accountId, command: `select ${mainHandItemId}` });
+  const { ResultString: encodedPrefab } = (await voodoo.command({
+    accountId,
+    command: 'select tostring'
+  })) as { ResultString: string };
+
+  /* Decode and parse the prefab string. */
+  const decodedString = decodeString(encodedPrefab);
+  const mainHandItem = parsePrefab(decodedString);
+
+  if (mainHandItem !== 'hilted apparatus') return;
+
+  /* Repair weapon. */
+  const hiltedApparatus = decodedString.prefab;
   const repairAmount = attributes.reconstructor / 100;
   const repairedApparatus = repairMaterial(hiltedApparatus, PhysicalMaterialPartHash.Iron, repairAmount);
 
   spawn(voodoo, accountId, {
     ...repairedApparatus,
-    prefabObject: {
-      ...repairedApparatus.prefabObject,
-      position,
-      rotation
-    },
     components: {
       ...repairedApparatus.components,
       NetworkRigidbody: {
-        position,
-        rotation
+        ...repairedApparatus.components?.NetworkRigidbody,
+        isKinematic: true
       }
     }
   });
